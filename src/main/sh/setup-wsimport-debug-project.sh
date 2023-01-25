afe392e0d1b9163d5214046a5f00e6bdfb641770
John Hartley <zebity@yahoo.com>
2023-01-26 01:27:54 +1100

    Test fresh start

--- src/main/sh/setup-wsimport-debug-project.sh
+++ src/main/sh/setup-wsimport-debug-project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VERSION=3.0.2-onvif-jak
+# VERSION=4.0.0-onvif-jak
 WSCOMPILE=wscompile
 PROJECT_ROOT=~/Documents/dev/onvif-relay
 
@@ -15,7 +16,7 @@ cd ${PROJECT_ROOT}
 cd .. && ( git clone https://github.com/zebity/metro-jax-ws.git )
 cd ${PROJECT_ROOT}/../metro-jax-ws
 git checkout ${VERSION}
-cd jaxws-ri/tools/wscompile
+cd jaxws-ri
 mvn package
 
 else
@@ -25,9 +26,10 @@ else
 
    cd ${PROJECT_ROOT}/..
    mvn archetype:generate -DgroupId=com.sun.${WSCOMPILE} -DartifactId=${WSCOMPILE} -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
-   cp -R metro-jax-ws/jaxws-ri/tools/wscompile/src/main/java metro-jax-ws/jaxws-ri/tools/wscompile/src/main/resources ${WSCOMPILE}/src/main
+   cp -R metro-jax-ws/jaxws-ri/tools/wscompile/src/main ${WSCOMPILE}/src
    mkdir -p ${WSCOMPILE}/target/generated-sources && cp -R metro-jax-ws/jaxws-ri/tools/wscompile/target/generated-sources/rsrc-gen ${WSCOMPILE}/target/generated-sources
    cp ${PROJECT_ROOT}/doco/pom.xml.debug-wsimport-${VERSION} ${WSCOMPILE}/pom.xml
+   mv ${PROJECT_ROOT}/../${WSCOMPILE}/src/main/java/module-info.java ${PROJECT_ROOT}/../${WSCOMPILE}/src/main/java/module-info.java.bak
  fi
 
 fi
