f49b8630196ab2f94fea8568c5b27afc121f3fd0
John Hartley <zebity@yahoo.com>
2023-12-12 14:48:37 +1100

    Constrain WD-Discovery probe to ONVIF Cameras (NetworkVideoTransmitter)

--- onvif-discovery-client/onvif-jak-discovery-client/src/main/java/onvif_relay/discovery_client/jak/JakOnvifDiscoveryClient.java
+++ onvif-discovery-client/onvif-jak-discovery-client/src/main/java/onvif_relay/discovery_client/jak/JakOnvifDiscoveryClient.java
@@ -5,6 +5,7 @@
 package onvif_relay.discovery_client.jak;
 
 import java.util.List;
+import javax.xml.namespace.QName;
 import jakarta.xml.ws.EndpointReference;
 import org.apache.cxf.ws.discovery.WSDiscoveryClient;
 
@@ -14,28 +15,25 @@ public class JakOnvifDiscoveryClient {
   public JakOnvifDiscoveryClient() {
   }
 
-  public void probe() {
-
+  public List<EndpointReference> probe(QName type) {
+    List<EndpointReference> res = null;
+    
     try (WSDiscoveryClient client = new WSDiscoveryClient()) {
       client.setVersion10();
       client.setDefaultProbeTimeout(1000);
       
-      System.out.println("Probe on: '" + client.getAddress() + "'.");
+      // System.out.println("Probe on: '" + client.getAddress() + "'.");
       // or: new WSDiscoveryClient("soap.udp://proxyhost:3702");
       // List<EndpointReference> references = client.probe(new QName("http://cxf.apache.org/hello_world/discovery", "Greeter"));
-      List<EndpointReference> references = client.probe();
+      // QName type = new QName("http://www.onvif.org/ver10/network/wsdl", "NetworkVideoTransmitter");
+      res = client.probe(type);
          
       // GreeterService service = new GreeterService();
       //loop through all of them and have them greet me.
       
-      System.out.println("Probe, got: " + references.size());
-      for (EndpointReference ref : references) {
-        // Greeter g = service.getPort(ref, Greeter.class);
-        // System.out.println(g.greetMe("World"));
-    	System.out.println("Ref: '" + ref.toString() + "'.");
-      }
     } catch (Exception ex) {
       System.out.println(ex);
     }
-  } 
+    return res;
+  }
 }
--- onvif-discovery-client/onvif-jak-discovery-client/src/test/java/onvif_relay/test/discovery_client/TestJakOnvifDiscoveryClient.java
+++ onvif-discovery-client/onvif-jak-discovery-client/src/test/java/onvif_relay/test/discovery_client/TestJakOnvifDiscoveryClient.java
@@ -5,39 +5,32 @@
 
 package onvif_relay.test.discovery_client;
 
-import fence.util.ConfigurationData;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import jakarta.xml.ws.EndpointReference;
 import onvif_relay.discovery_client.jak.JakOnvifDiscoveryClient;
 
 public class TestJakOnvifDiscoveryClient {
 	
   public static void main(String [] args) {
-	// ConfigurationData conf = new ConfigurationData(args);
 	
     try {
       System.out.println("Running test ...");
       
-      // String ipv4 = conf.getItem("onvif-device", "device");
-      
       JakOnvifDiscoveryClient onvdis = new JakOnvifDiscoveryClient();
-      onvdis.probe();
+      QName type = new QName("http://www.onvif.org/ver10/network/wsdl", "NetworkVideoTransmitter");
       
-      // String got = onv.getInfo(conf, ipv4, "test#1", "Device", "GetDeviceInformation", "{}");
+      List<EndpointReference> found = onvdis.probe(type);
       
-      // System.out.println("TestJakOnvifClient::main - got: \"" + got + "\".");
-      
-      // ser.toJson(devs, new FileWriter(dumpFile));
-      // ser.toJson(UIDGen, new FileWriter(dumpFile));
-      /* String peek = ser.toJson(ipAddr);
-      System.out.println(peek);
-      
-      peek = ser.toJson(dev);
-      System.out.println(peek);
-      
-      DeviceInfo deserDev = new Gson().fromJson(peek, DeviceInfo.class);
-      
-      peek = ser.toJson(deserDev);
-      System.out.println(peek); */
-      
+      System.out.println("Probe, got: " + found.size());
+      for (EndpointReference ref : found) {
+        // Greeter g = service.getPort(ref, Greeter.class);
+        // System.out.println(g.greetMe("World"));
+    	System.out.println("Found: '" + ref.toString() + "'.");
+      }
+
 	} catch (Exception x) {
       x.printStackTrace();
 	}
